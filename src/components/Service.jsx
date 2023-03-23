57330c83e046c0bc7a1cf113988d8071e39e99d0
Jason Huang <jason97521@gmail.com>
2023-03-23 02:47:56 -0400

    adding react video to service page

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import How from "../img/how.png";
 import MiniCard from './MiniCard';
@@ -12,9 +12,21 @@ const Left = styled.div`
     width: 50%;
     display: flex;
     align-items: center;
+    position: relative;
 `
 const Image = styled.img`
-    height: 100%;    
+    display: ${(props)=> props.open && "none"};
+    height: 100%;
+    margin-left: 100px;
+`
+const Video = styled.video`
+    display: ${(props)=> !props.open && "none"};
+    height: 300px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
 `
 const Right = styled.div`
     width: 50%;
@@ -56,10 +68,12 @@ const Icon = styled.img`
 `
 
 const Service = () => {
+    const [open,setOpen] = useState(false)
   return (
     <Container>
         <Left>
-            <Image src={How} />
+            <Image open={open} src={How} />
+            <Video open={open} autoPlay loop controls src="https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761"/>
         </Left>
         <Right>
             <Wrapper>
@@ -72,7 +86,10 @@ const Service = () => {
                     <MiniCard/>
                     <MiniCard/>
                 </CardContainer>
-                <Button><Icon src={Play}/>How it works</Button>
+                <Button onClick={()=>setOpen(true)}>
+                    <Icon src={Play}/>
+                    How it works
+                </Button>
             </Wrapper>
         </Right>
     </Container>
