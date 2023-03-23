fd2625bdeaf6a9de4fc4a7e55e5f593baa1fceff
Jason Huang <jason97521@gmail.com>
2023-03-23 03:07:20 -0400

    Pricing Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components"
 import Feature from "./components/Feature";
 import Intro from "./components/Intro";
 import Navbar from "./components/Navbar";
+import Price from "./components/Price";
 import Service from "./components/Service";
 
 const Container = styled.div`
@@ -32,6 +33,11 @@ const ServiceShape = styled.div`
   clip-path: polygon(0% 0%, 33% 0%, 33% 100%, 0% 100%);
   background-color: #f88497;
 `
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0%, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
+`
 
 const App = () => {
   return (
@@ -48,6 +54,10 @@ const App = () => {
       <Container>
         <Service/>
         <ServiceShape/>
+      </Container>
+      <Container>
+        <Price/>
+        <PriceShape/>
       </Container>
     </>
   )
